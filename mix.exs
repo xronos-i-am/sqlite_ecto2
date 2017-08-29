d2a53a6fc0a8587150e7693aa65f0efae5223078
Eric Scouten <eric@scouten.com>
2017-08-28 22:22:19 -0700

    Track GitHub releases of Ecto in this branch.

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Sqlite.Ecto2.Mixfile do
      {:dogma, "~> 0.1", only: :dev},
      {:esqlite, "~> 0.2.3"},
      {:ex_doc, "~> 0.16", only: :dev},
-     {:ecto, "~> 2.2.0"},
+     {:ecto, github: "elixir-ecto/ecto", ref: "394cb70d75d71a49886cab0f314d93022f1c1008"},
      {:poison, "~> 2.2 or ~> 3.0", optional: true},
      {:postgrex, "~> 0.13.0", optional: true},
      {:sbroker, "~> 1.0"},
